fc8323953eb65b757d8f35df7fa6b98238be95e6
David THIBAU <david.thibau@gmail.com>
2018-11-23 08:58:34 +0100

    Settings routes and proxy

--- members-service/src/main/java/org/formation/MembersServer.java
+++ members-service/src/main/java/org/formation/MembersServer.java
@@ -3,6 +3,7 @@ package org.formation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
@@ -18,6 +19,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MembersServer {
 
 
