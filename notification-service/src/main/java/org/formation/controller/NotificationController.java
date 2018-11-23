7f5b3525a92cd0c50dbd243b972eb286f6982f0e
David THIBAU <david.thibau@gmail.com>
2018-11-23 12:46:39 +0100

    Adding Sleuth and Zipkin

--- notification-service/src/main/java/org/formation/controller/NotificationController.java
+++ notification-service/src/main/java/org/formation/controller/NotificationController.java
@@ -1,5 +1,7 @@
 package org.formation.controller;
 
+import java.util.logging.Logger;
+
 import org.formation.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,11 +16,15 @@ public class NotificationController {
 	@Autowired
 	EmailService emailService;
 	
+	protected Logger logger = Logger.getLogger(NotificationController.class.getName());
+	
 	@RequestMapping(path = "/sendSimple", method = RequestMethod.POST)
 	public String sendSimpleMessage(@RequestBody Email email) {
 	
+		logger.info("About to send a mail");
 		emailService.sendSimpleMessage(email.getTo(), email.getSubject(), email.getText());
 		
+		logger.info("Mail sent");
 		return "OK";
 		
 	}
--- notification-service/src/main/java/org/formation/service/EmailServiceImpl.java
+++ notification-service/src/main/java/org/formation/service/EmailServiceImpl.java
@@ -1,5 +1,8 @@
 package org.formation.service;
 
+import java.util.logging.Logger;
+
+import org.formation.controller.NotificationController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,15 +13,20 @@ public class EmailServiceImpl implements EmailService {
 	  
     @Autowired
     public JavaMailSender emailSender;
+    
+    protected Logger logger = Logger.getLogger(NotificationController.class.getName());
  
     public void sendSimpleMessage(
       String to, String subject, String text) {
-
-        SimpleMailMessage message = new SimpleMailMessage(); 
+    	
+    	logger.info("MailService entering");
+    	SimpleMailMessage message = new SimpleMailMessage(); 
         message.setTo(to); 
         message.setSubject(subject); 
         message.setText(text);
         emailSender.send(message);
+        
+        logger.info("MailService sent a mail : "+ message);
     }
 }
 
