3e8be23cd5a84f112a1dacd710543dbb5ce53cce
David THIBAU <david.thibau@gmail.com>
2019-10-02 17:46:40 +0200

    No EnableEureka

--- document-service/src/main/java/org/formation/document/DocumentServiceApplication.java
+++ document-service/src/main/java/org/formation/document/DocumentServiceApplication.java
@@ -2,10 +2,8 @@ package org.formation.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DocumentServiceApplication {
 
 	public static void main(String[] args) {
