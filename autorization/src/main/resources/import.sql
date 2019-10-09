c1fb49c5b4e6e2b20e896a0ca624a4d580ff6fdb
David THIBAU <david.thibau@gmail.com>
2019-10-09 22:10:09 +0200

    oAuh2 with ACLs

---
+++ autorization/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('dthibau@wmmod.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','THIBAU','David',49);
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('dthibau@mymeetingsondemand.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','THIBAU','Michel',52);
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('david.thibau@gmail.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','THIBAU','Gaston',67);
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('toto@titi.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','TITI','Toto',40);
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('tutu@tata.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','TATA','Tutu',29);
+insert into MEMBER (EMAIL, PASSWORD, NOM, PRENOM, AGE) values ('tete@tonton.com', '$2a$04$54PNEzvW7t8n14wmLEJukepUg3Qx8HJ35H2b8hUo2F1oCgGf5589W','TONTON','Tete',88);
+
+
+
+
