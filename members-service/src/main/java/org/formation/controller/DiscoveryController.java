928967457adbcf3750f422e61c7595d111b46f3e
David THIBAU <david.thibau@gmail.com>
2020-06-03 15:18:11 +0200

    RestTemplate et FQDN

--- members-service/src/main/java/org/formation/controller/DiscoveryController.java
+++ members-service/src/main/java/org/formation/controller/DiscoveryController.java
@@ -1,21 +1,47 @@
 package org.formation.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/members/discovery")
+@RequestMapping("/members")
 public class DiscoveryController {
 
 	@Autowired
 	DiscoveryClient discoveryClient;
 	
+	@Autowired
+	RestTemplateBuilder restBuilder;
+	
+	private static String NOTIFICATION_SERVICE="http://notification-service.default.svc.cluster.local:9090";
+	
 	@GetMapping
-	public Object getInstance(@RequestParam String serviceName) {
+	@RequestMapping("/discovery")
+	public List<ServiceInstance> getInstance(@RequestParam String serviceName) {
 		return discoveryClient.getInstances(serviceName);
+	}
+	
+	@GetMapping
+	@RequestMapping("/rest")
+	public ResponseEntity<String> callNotification() {
+		RestTemplate restTemplate = restBuilder.rootUri(NOTIFICATION_SERVICE).build();
+		Map<String,String> map = new HashMap<>();
+		map.put("subject", "Sujet");
+		map.put("to", "david.thibau@gmail.com");
+		map.put("text", "Un coucou");
+		
+		return restTemplate.postForEntity("/sendSimple", map, String.class);
 	}
 }
