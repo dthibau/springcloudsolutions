cae78ad9f5aceb0d45efe9fa00ae494394be88ad
David THIBAU <david.thibau@gmail.com>
2020-06-03 09:49:26 +0200

    ClusterRole et ConfigMap

---
+++ config/deployment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kubectl delete rolebinding default:service-discovery-client
+kubectl delete configmap application-config 
+kubectl delete configmap notification-config 
+
+kubectl create rolebinding default:service-discovery-client --clusterrole service-discovery-client --serviceaccount default:default
+kubectl create configmap application-config --from-file=./src/main/resources/shared/annuaire.yml
+kubectl create configmap notification-config --from-file=./src/main/resources/shared/notification-service.yml
