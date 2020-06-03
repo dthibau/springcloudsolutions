a5889bb1e9e3d6d39d2dbe54d6a2cfcc33966b2f
David THIBAU <david.thibau@gmail.com>
2020-06-03 10:31:39 +0200

    Fix application-config

--- config/deployment.sh
+++ config/deployment.sh
@@ -5,5 +5,5 @@ kubectl delete configmap application-config
 kubectl delete configmap notification-config 
 
 kubectl create rolebinding default:service-discovery-client --clusterrole service-discovery-client --serviceaccount default:default
-kubectl create configmap application-config --from-file=./src/main/resources/shared/annuaire.yml
+kubectl create configmap application-config --from-file=./src/main/resources/shared/application.yml
 kubectl create configmap notification-config --from-file=./src/main/resources/shared/notification-service.yml
