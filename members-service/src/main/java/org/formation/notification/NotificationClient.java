139e63970c6d8f817272573207b73924fe273b1d
David THIBAU <david.thibau@gmail.com>
2020-06-03 16:40:50 +0200

    Ajout de Hystrix

--- members-service/src/main/java/org/formation/notification/NotificationClient.java
+++ members-service/src/main/java/org/formation/notification/NotificationClient.java
@@ -5,6 +5,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @Service
 public class NotificationClient {
 
@@ -14,11 +17,20 @@ public class NotificationClient {
 	
 	private static String NOTIFICATION_SERVICE="http://notification-service.default.svc.cluster.local:9090";
 
-	
+	@HystrixCommand(fallbackMethod = "envoiCourrierFallBack",
+			commandProperties = { @HystrixProperty(
+			name = "execution.isolation.thread.timeoutInMilliseconds",
+			value = "1000") })
 	public String envoiCourrier(Courriel courriel) {
 		RestTemplate restTemplate = restBuilder.rootUri(NOTIFICATION_SERVICE).build();
 
 		
 		return restTemplate.postForEntity("/sendSimple", courriel, String.class).getBody();
+	}
+	
+	public String envoiCourrierFallBack(Courriel courriel) {
+		
+		System.out.println("FALLBACK "+courriel.getTo());
+		return "NOT SEND YET";
 	}
 }
