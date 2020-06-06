f5736a318bc469c48f82441a4c550f40c9c24f86
David THIBAU <david.thibau@gmail.com>
2020-06-06 11:28:29 +0200

    Kub. deployment de documents-service

--- config/deployment.sh
+++ config/deployment.sh
@@ -5,11 +5,13 @@ kubectl delete secret smtp-secret
 kubectl delete configmap application-config 
 kubectl delete configmap notification-config 
 kubectl delete configmap members-config 
+kubectl delete configmap documents-config 
 
 kubectl create rolebinding default:service-discovery-client --clusterrole service-discovery-client --serviceaccount default:default
 kubectl create secret generic smtp-secret --from-literal=username=toto@titi.com --from-literal=password=secret
 kubectl create configmap application-config --from-file=./src/main/resources/shared/application.yml
 kubectl create configmap notification-config --from-file=./src/main/resources/shared/notification-service.yml
 kubectl create configmap members-config --from-file=./src/main/resources/shared/members-service.yml
+kubectl create configmap documents-config --from-file=./src/main/resources/shared/documents-service.yml
 
 
